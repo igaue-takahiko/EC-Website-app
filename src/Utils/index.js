2e8de592f686f25c9543789289e54fd87cfe8935
igaue-takahhiko <hasewo.2610@gmail.com>
2020-12-14 12:06:00 +0900

    implement cusomHooks useAuth useAdminAuth

---
+++ src/Utils/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const checkUserIsAdmin = (currentUser) => {
+    if (!currentUser || !Array.isArray(currentUser.userRoles)) {
+        return false
+    }
+    const { userRoles } = currentUser;
+    if (userRoles.includes("admin")) {
+        return true
+    }
+    return false
+}
+
+export const apiInstance = axios.create({
+    baseURL: ''
+})
---
+++ src/customHooks/index.js
@@ -0,0 +1,2 @@
+export { default as useAdminAuth } from './useAdminAuth'
+export { default as useAuth } from './useAuth'
